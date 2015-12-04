0b2e3031ee6150a11f9def7ebbd1f8cb9bf9cf2f
Clyde Velasquez <clydevelasquez@outlook.com>
2015-12-04 12:14:42 +0800

    First Commit

---
+++ src/com/clyde/image/JpegImageAnalysis.java
@@ -0,0 +1,13 @@
+package com.clyde.image;
+
+/**
+ * Class for analysing jpeg images
+ *
+ *
+ * @author Clyde M. Velasquez
+ * @version 0.1
+ * @since December 03, 2015
+ */
+public class JpegImageAnalysis {
+
+}
