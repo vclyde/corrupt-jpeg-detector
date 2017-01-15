395356373540ba430a3a53ef94c661e2799866c3
clydeev <clydevelasquez@outlook.com>
2017-01-15 23:33:11 +0800

    Make resources directory a static and final variable

--- src/test/java/com/clyde/util/CorruptJPEGDetectorTest.java
+++ src/test/java/com/clyde/util/CorruptJPEGDetectorTest.java
@@ -1,87 +1,90 @@
 package com.clyde.util;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for CorruptJPEGDetector class
  *
  * @author Clyde M. Velasquez
- * @version 1.0
+ * @version 1.1
  * @since 10/24/16.
  */
 public class CorruptJPEGDetectorTest {
     private static final File ROOT_DIR = new File("").getAbsoluteFile();
+    private static final File RESOURCES_DIR = new File(ROOT_DIR, "src/main/resources/");
+
+    @Before
+    public void init() {
+        assertTrue(RESOURCES_DIR.exists());
+    }
 
     @Test
     public void isJPEG() throws Exception {
-        File resources = new File(ROOT_DIR, "src/main/resources/");
-        assertTrue(resources.exists());
-
-        File[] testFiles = resources.listFiles((dir, name) -> !name.contains(".png") && !name.contains(".gif"));
+        // Only jpeg files
+        File[] testFiles = RESOURCES_DIR.listFiles((dir, name) -> !name.contains(".png") && !name.contains(".gif"));
         CorruptJPEGDetector cjd;
-        assert testFiles != null;
+
+        assertNotNull(testFiles);
         for (File jpg : testFiles) {
             cjd = new CorruptJPEGDetector(jpg);
             assertTrue(cjd.isJPEG());
         }
 
-        File png = new File(resources, "image6.png");
+        File png = new File(RESOURCES_DIR, "image6.png");
         cjd = new CorruptJPEGDetector(png, true);
         assertFalse(cjd.isJPEG());
     }
 
     @Test
     public void isFileComplete() throws Exception {
-        File resources = new File(ROOT_DIR, "src/main/resources/");
-
-        File image1 = new File(resources, "image1.jpg");
+        File image1 = new File(RESOURCES_DIR, "image1.jpg");
         CorruptJPEGDetector cjd = new CorruptJPEGDetector(image1);
         assertFalse(cjd.isFileComplete());
 
-        File image2 = new File(resources, "image2.jpg");
+        File image2 = new File(RESOURCES_DIR, "image2.jpg");
         cjd = new CorruptJPEGDetector(image2);
         assertTrue(cjd.isFileComplete());
 
-        File image3 = new File(resources, "image3.jpg");
+        File image3 = new File(RESOURCES_DIR, "image3.jpg");
         cjd = new CorruptJPEGDetector(image3);
         assertTrue(cjd.isFileComplete());
     }
 
     @Test
     public void isCorrupt() throws Exception {
-        File resources = new File(ROOT_DIR, "src/main/resources/");
-
-        File image1 = new File(resources, "image1.jpg");
+        File image1 = new File(RESOURCES_DIR, "image1.jpg");
         CorruptJPEGDetector cjd = new CorruptJPEGDetector(image1);
         assertTrue(cjd.isCorrupt());
 
-        File image2 = new File(resources, "image2.jpg");
+        File image2 = new File(RESOURCES_DIR, "image2.jpg");
         cjd = new CorruptJPEGDetector(image2);
         assertTrue(cjd.isCorrupt());
 
-        File image3 = new File(resources, "image3.jpg");
+        File image3 = new File(RESOURCES_DIR, "image3.jpg");
         cjd = new CorruptJPEGDetector(image3);
         assertFalse(cjd.isCorrupt());
 
-        File image4 = new File(resources, "image4.jpg");
+        File image4 = new File(RESOURCES_DIR, "image4.jpg");
         cjd = new CorruptJPEGDetector(image4);
         assertTrue(cjd.isCorrupt());
 
-        File image5 = new File(resources, "image5.jpg");
+        File image5 = new File(RESOURCES_DIR, "image5.jpg");
         cjd = new CorruptJPEGDetector(image5);
         assertTrue(cjd.isCorrupt());
     }
 
     @Test
     public void printHexDump() throws Exception {
-        File resources = new File(ROOT_DIR, "src/main/resources");
-        File image3 = new File(resources, "image3.jpg");
+        File image3 = new File(RESOURCES_DIR, "image3.jpg");
         CorruptJPEGDetector cjd = new CorruptJPEGDetector(image3);
-        cjd.printHexDump();
+        assertNotNull(cjd.getHexDump());
+        assertTrue(!cjd.getHexDump().equals(""));
     }
 }
